3244e187f0cf371e747fd90fa17b25219aacd4ac
Pedro Melendez <elpeme@gmail.com>
2021-11-09 00:19:56 -0800

    Added some doc strings

--- main.py
+++ main.py
@@ -12,26 +12,40 @@ from google.protobuf import field_mask_pb2
 log_policy = True
 
 
-def identify_log_message(event, context):
+def main_function(event, context):
     """
-    Triggered from a message on a Cloud Pub/Sub topic. Executes functions based on the log's 'Method'
+    Triggered from a message on a Cloud Pub/Sub topic.
+    Captures method, region from event['data']['resource']['labels']['method']
     :param event:  Event payload (dict)
     :param context: Metadata for the event (google.cloud.functions.Context)
     :return:
     """
 
-    # Decode Data from event and capture log method from ['data']['resource']['labels']['method']
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
     pubsub_json = json.loads(pubsub_message)
+
+    # Get event method and region
     log_method = pubsub_json['resource']['labels']['method']
     region = pubsub_json['protoPayload']['resourceLocation']['currentLocations'][0]
     client_options = get_client_option(region)
+    execute_policy_enforcer(log_method, client_options)
+
+
+def execute_policy_enforcer(log_method, client_options, pubsub_json):
+    """
+    Executes functions based on the log's 'Method'
+    :param log_method: Log's method captured (str)
+    :param client_options: API client options (client_options)
+    :param pubsub_json: JSON including the pub/sub message (dict)
+    :return: Modified object based on the log's method
+    """
 
     # Remove webhook credentials after an update
     if "Webhooks.UpdateWebhook" in log_method:
         webhook_name = pubsub_json['protoPayload']['resourceName']
-        delete_webhook_credentials(webhook_name, client_options)
-        print('Deleted static credentials on Webhook: ' + str(webhook_name) + 'inform end user')
+        webhook_updated = delete_webhook_credentials(webhook_name, client_options)
+        print('Deleted static credentials on Webhook: ' + str(webhook_name))
+        return webhook_updated
 
     # Remove webhook credentials after a new Webhook is created
     elif "Webhooks.CreateWebhook" in log_method:
@@ -39,6 +53,14 @@ def identify_log_message(event, context):
         enforced_webhooks = webhook_cred_enforcer(agent_id, client_options)
         for webhook in enforced_webhooks:
             print('Deleted static credentials on Webhook: ' + str(webhook.name))
+        return enforced_webhooks
+
+    # Set correct log policy after agent is updated
+    elif "Agents.UpdateAgent" in log_method:
+        agent_id = pubsub_json['protoPayload']['resourceName']
+        enforced_agent = enforce_agent_logging(agent_id, log_policy, client_options)
+        print('Updated Dialogflow log policy to ' + str(log_policy) + ' on Dialogflow agent: ' + enforced_agent.name)
+        return enforced_agent
 
     # Set correct log policy after agent is created
     elif "Agents.CreateAgent" in log_method:
@@ -47,16 +69,12 @@ def identify_log_message(event, context):
         enforced_agents = [enforce_agent_logging(agent.name, log_policy, client_options) for agent in agents]
         for agent in enforced_agents:
             print('Updated Dialogflow log policy ' + str(log_policy) + ' on Dialogflow Agent: ' + agent.name)
-
-    # Set correct log policy after agent is updated
-    elif "Agents.UpdateAgent" in log_method:
-        agent_id = pubsub_json['protoPayload']['resourceName']
-        enforced_agent = enforce_agent_logging(agent_id, log_policy, client_options)
-        print('Updated Dialogflow log policy to ' + str(log_policy) + ' on Dialogflow agent: ' + enforced_agent.name)
+        return enforced_agents
 
     else:
         print(log_method)
         print('No logs matched. Nothing changed')
+        return False
 
 
 def get_client_option(region):
@@ -78,7 +96,7 @@ def get_client_option(region):
 def enforce_agent_logging(name, policy, client_options):
     """
     Used to enforce the logging policy on the agent.
-    :param client_options:
+    :param client_options: API client options
     :param name: Dialogflow Agent ID (str)
     :param policy: Dialogflow Logging policy required (bool)
     :return: agent object with modified logging settings
@@ -110,7 +128,7 @@ def enforce_agent_logging(name, policy, client_options):
 def delete_webhook_credentials(webhook_name, client_options):
     """ Returns a webhook object without credentials
     Args:
-        :param client_options:
+        :param client_options: API client options
         :param webhook_name: Dialogflow Webhook name (str)
     """
 
@@ -136,11 +154,10 @@ def delete_webhook_credentials(webhook_name, client_options):
 def webhook_cred_enforcer(agent_id, client_options):
     """ Removes static credentials from all webhooks of the agent_id
     Args:
-         agent_id (str): Dialogflow agent id
-         :param client_options:
+         :param agent_id: Dialogflow agent id (str):
+         :param client_options: API client options
 
     """
-
     webhook_client = WebhooksClient(client_options=client_options)
     webhooks = webhook_client.list_webhooks(parent=agent_id)
     modified_webhooks = [delete_webhook_credentials(webhook.name, client_options) for webhook in webhooks]
@@ -151,7 +168,7 @@ def list_agents(parent, client_options):
     """ Returns a ListAgentsPager object with the CX agents created on the project_id
     Args:
         :param parent: Dialogflow agent location Format: projects/<Project ID>/locations/<Location ID> (str)
-        :param client_options:
+        :param client_options: API client options
     """
 
     # Creates Dialogflow API Client
