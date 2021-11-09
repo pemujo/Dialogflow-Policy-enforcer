6c3dfdaf173c4074e2cea1efb02ef6b6022738aa
Pedro Melendez <elpeme@gmail.com>
2021-11-09 14:31:01 -0800

    Copyright disclaimer

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# Copyright 2021 Google LLC
+#
+# This software is provided as-is, without warranty or representation for any
+# use or purpose. Your use of it is subject to your agreement with Google.
+
 import base64
 import json
 from google.cloud.dialogflowcx_v3.services.agents.client import AgentsClient
